c900bd347bf8ce33d1328f9faf6d752a2ca5f7bd
maddoxman5000 <maddox.jacko99@gmail.com>
2023-12-09 16:42:26 +0100

    Added a game loop.

--- __main__.py
+++ __main__.py
@@ -1,9 +1,6 @@
 import pygame
 
-# for loop traversing from 1 to 4
-for i in range(1, 5):
-  # If this condition becomes true break will execute
-    if(i == 3):
-     # Break statement will terminate the entire loop
-        break
-    print(i)
+pygame.init()
+
+while True:
+    pass
